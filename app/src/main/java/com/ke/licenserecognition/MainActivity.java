4296e2e8e64766a3f396d82dc28eb9af5714bb87
yuyanhui <913918146@qq.com>
2017-10-30 17:33:15 +0800

    implementation 替换成 compile

--- app/src/main/java/com/ke/licenserecognition/MainActivity.java
+++ app/src/main/java/com/ke/licenserecognition/MainActivity.java
@@ -1,7 +1,14 @@
 package com.ke.licenserecognition;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kernal.plateid.RecogService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent authIntent = new Intent(this,
+                RecogService.class);
+        bindService(authIntent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
 
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        }, Service.BIND_AUTO_CREATE);
     }
 }
